9640936c95a23e8611d5c0ff59b7df58300a866f
Fei Wang <99712213+fei-tripalink@users.noreply.github.com>
2022-08-07 17:42:48 +0800

    chore : add Consul for microservice

--- src/main/java/com/fei/Application.java
+++ src/main/java/com/fei/Application.java
@@ -18,10 +18,12 @@ package com.fei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
