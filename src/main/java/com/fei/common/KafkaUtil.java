62531979460128f48fa9a799d3e8ef947d482ad0
Fei Wang <99712213+fei-tripalink@users.noreply.github.com>
2022-08-23 20:36:34 +0800

    kafka manual offset

--- src/main/java/com/fei/common/KafkaUtil.java
+++ src/main/java/com/fei/common/KafkaUtil.java
@@ -1,7 +1,10 @@
 package com.fei.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Component
+@Slf4j
 public class KafkaUtil {
 
     @Resource
@@ -19,12 +23,32 @@ public class KafkaUtil {
         kafkaTemplate.send(topic, msg);
     }
 
+    /**
+     * 自动ack
+     */
 //    @KafkaListener(topics = "wj0jddu6-default")
-    @KafkaListener(topics = "${cloudkarafka.topic}")
-    public void processMessage(String message,
-                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
-                               @Header(KafkaHeaders.RECEIVED_TOPIC) List<String> topics,
-                               @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
-        System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
+//    @KafkaListener(topics = "${cloudkarafka.topic}")
+//    public void processMessage(String message,
+//                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
+//                               @Header(KafkaHeaders.RECEIVED_TOPIC) List<String> topics,
+//                               @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+//        System.out.printf("%s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
+//    }
+
+    /**
+     * 手动ack
+     * 需要KafkaConfig文件的配合
+     */
+    @KafkaListener(topics = "${cloudkarafka.topic}", containerFactory = "ackContainerFactory")
+    public void handleMessage(ConsumerRecord record, Acknowledgment acknowledgment) {
+        try {
+            String message = (String) record.value();
+            log.info("收到消息: {}", message);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            // 手动提交 offset
+            acknowledgment.acknowledge();
+        }
     }
 }
